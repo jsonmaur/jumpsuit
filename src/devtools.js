d64ab18f733d581a8f013c7fd59e693b5b229dee
Tanner Linsley <tannerlinsley@gmail.com>
2016-05-07 03:19:53 -0500

    Hotwire the DockMonitor

--- src/devtools.js
+++ src/devtools.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, {Component, PropTypes} from 'react'
 import * as DevTools from 'redux-devtools'
 import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
 
 export const devTools = DevTools
 
+class JumpsuitDockMonitor extends DockMonitor {
+  componentDidMount(){
+    getDevToolsState = () => this.props.store.getState()
+    setDevToolsState = (state) => {
+      this.props.dispatch({
+        type: 'IMPORT_STATE',
+        nextLiftedState: state
+      })
+    }
+  }
+}
+
 export default DevTools.createDevTools(
-  <DockMonitor
+  <JumpsuitDockMonitor
     toggleVisibilityKey='ctrl-h'
     changePositionKey='ctrl-q'
     defaultIsVisible={ true }>
     <LogMonitor theme='tomorrow' />
-  </DockMonitor>
+  </JumpsuitDockMonitor>
 )
+
+export function getDevToolsState(){}
+export function setDevToolsState(){}
