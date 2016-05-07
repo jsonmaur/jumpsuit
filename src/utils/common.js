d272dfcd559082581e5ca620c7175731058c8f9e
Jason Maurer <j@maur.co>
2016-05-07 15:28:25 -0500

    common functions and tests

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,22 +1,92 @@
 import net from 'net'
 
-export function debounce (func, wait) {
+/**
+ * Gets a variable type without using typeof.
+ * @param {any} value - The value to analyze
+ * @return {string} The var type
+ */
+export function getType (value) {
+  return ({}).toString.call(value).match(/\s([^\]]+)/)[1].toLowerCase()
+}
+
+/**
+ * Checks the data type of a value and throws type errors.
+ * @param {any} value - The value to check type on
+ * @param {string|array} type - The type(s) to use
+ * @return {boolean} Whether the type check passed
+ */
+export function typeCheck (value, types, options = {}) {
+  if (options.throw === undefined) options.throw = true
+
+  const typesType = getType(types)
+  if (!types || !typesType.match(/string|array/)) {
+    throw new Error('typeCheck type param must be a string or array')
+  }
+
+  if (typesType !== 'array') types = [types]
+
+  const valueType = getType(value)
+  const valueStr = valueType === 'string' ? `"${value}"` : value
+  const typeStr = types.length > 1 ? `{${types.join(', ')}}` : types[0]
+
+  if (value && types.indexOf(valueType) === -1) {
+    const error = `value ${valueStr} should be a ${typeStr} but got ${valueType}`
+
+    if (options.throw) throw new TypeError(error)
+    return false
+  }
+
+  return true
+}
+
+/**
+ * Gets a value in a deep object with the path defined as a string.
+ * @param {object} obj - The object to search in
+ * @param {string} path - The path to search
+ * @return {any} The value found at the path
+ */
+export function deepGet (obj = {}, path = '') {
+  typeCheck(obj, 'object')
+  typeCheck(path, 'string')
+
+  const paths = path.split('.')
+
+  let current = obj
+  for (const i in paths) {
+    if (current[paths[i]] === undefined) return
+    current = current[paths[i]]
+  }
+
+  return current
+}
+
+/**
+ * Only call a function once within a given timeframe,
+ * no matter how many times it is actually called.
+ * @param {function} fn - The functiont to call
+ * @param {integer} wait - The limit time (in ms)
+ * @return {function} The debounced function to call
+ */
+export function debounce (fn, wait = 100) {
+  typeCheck(fn, 'function')
+
   let timeout
-
   return function () {
-    const context = this
-    const args = arguments
-
     clearTimeout(timeout)
+
     timeout = setTimeout(() => {
       timeout = null
-      func.apply(context, args)
+      fn.apply(this, arguments)
     }, wait)
 
-    if (!timeout) func.apply(context, args)
+    if (!timeout) fn.apply(this, arguments)
   }
 }
 
+/**
+ * Returns a port that is free on the host machine.
+ * @return {integer} A free port
+ */
 export function getFreePort () {
   return new Promise((resolve, reject) => {
     const server = net.createServer()
--- test/unit/utils/common.test.js
+++ test/unit/utils/common.test.js
@@ -1,3 +1,60 @@
 import test from 'ava'
+import sinon from 'sinon'
+import * as common from '../../../src/utils/common'
 
-test(() => {})
+test('typeCheck', (t) => {
+  t.throws(() => common.typeCheck(1, 'string'), TypeError)
+  t.notThrows(() => common.typeCheck(1, 'number'), TypeError)
+  t.throws(() => common.typeCheck('test', 'number'), TypeError)
+  t.notThrows(() => common.typeCheck('test', 'string'), TypeError)
+  t.throws(() => common.typeCheck('test', ['number', 'object']), TypeError)
+  t.notThrows(() => common.typeCheck('test', ['number', 'string']), TypeError)
+  t.false(common.typeCheck(1, 'string', { throw: false }))
+  t.true(common.typeCheck(1, 'number', { throw: false }))
+  t.false(common.typeCheck('test', 'number', { throw: false }))
+  t.true(common.typeCheck('test', 'string', { throw: false }))
+  t.false(common.typeCheck('test', ['number', 'object'], { throw: false }))
+  t.true(common.typeCheck('test', ['number', 'string'], { throw: false }))
+  t.throws(() => common.typeCheck(1), Error)
+})
+
+test('getType', (t) => {
+  t.is(common.getType('hello'), 'string')
+  t.is(common.getType(1), 'number')
+  t.is(common.getType({}), 'object')
+  t.is(common.getType([]), 'array')
+  t.is(common.getType(() => {}), 'function')
+  t.is(common.getType(function () {}), 'function')
+})
+
+test('deepGet', (t) => {
+  const shallowObj = { a: 'hi' }
+  const deepObj = { a: { b: { c: 'hi' } } }
+  t.is(common.deepGet(shallowObj, 'a'), 'hi')
+  t.is(common.deepGet(deepObj, 'a.b.c'), 'hi')
+  t.falsy(common.deepGet())
+  t.falsy(common.deepGet(deepObj, 'a.b.d'))
+  t.throws(() => common.deepGet('hi'), TypeError)
+  t.throws(() => common.deepGet(shallowObj, 1), TypeError)
+})
+
+test('debounce', (t) => {
+  const clock = sinon.useFakeTimers()
+  const spy = sinon.spy()
+  const bounce = common.debounce(spy, 100)
+  bounce()
+  bounce()
+  clock.tick(90)
+  bounce()
+  clock.tick(99)
+  bounce()
+  clock.tick(100)
+  t.is(spy.callCount, 1)
+  clock.restore()
+  t.throws(() => common.debounce('hi'))
+})
+
+test('getFreePort', async (t) => {
+  t.is(typeof await common.getFreePort(), 'number')
+  t.true(await common.getFreePort() > 0)
+})
