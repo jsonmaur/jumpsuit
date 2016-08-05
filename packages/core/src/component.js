8af9b32035150bcfb3185a4f4dcea8791482859e
Jason Maurer <j@maur.co>
2016-08-05 13:51:56 -0600

    component revert

--- packages/core/src/component.js
+++ packages/core/src/component.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-export default function (component, connectFn) {
-  const container = typeof component === 'object'
-    ? React.createClass(options)
-    : component
+export default function (options, connectFn) {
+  // TODO: allow component creation with a class
+  // see https://github.com/jumpsuit/jumpsuit/pull/18
 
+  const container = React.createClass(options)
   return connectFn ? connect(connectFn)(container) : container
 }
