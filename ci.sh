0b5492641781ce07d27c1373c0e01547782c6bb9
Tanner Linsley <tannerlinsley@gmail.com>
2016-11-09 12:25:23 -0700

    Better ci

--- ci.sh
+++ ci.sh
@@ -1,5 +1,5 @@
 npm run test
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false"];
-then npm run docs:publish;
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
+  then npm run docs:publish
 else echo 'No doc deployments on PRs. Sorry!'
-fi;
+fi
